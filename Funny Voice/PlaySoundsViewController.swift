cdddd1c8d881520e71249f626ba5ef8a1f55f5b2
Faisal-Manzer <faisal_manzer@Faisals-MacBook.local>
2018-04-23 00:09:47 +0530

    Added IBInlet and IBOutlet to play button

--- Funny Voice/PlaySoundsViewController.swift
+++ Funny Voice/PlaySoundsViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     var recordedAudioUrl: URL!
 
     override func viewDidLoad() {
@@ -23,6 +31,13 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
 
     /*
     // MARK: - Navigation
