8ce6014dc2a333a68cf3226c788900a4a121a4a7
Faisal-Manzer <faisal_manzer@Faisals-MacBook.local>
2018-04-22 12:09:45 +0530

    Recording btn function

--- Funny Voice/ViewController.swift
+++ Funny Voice/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordBtn: UIButton!
+    var isRecording = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordBtn(_ sender: Any) {
+        print("clicked btn")
+        if !isRecording {
+            recordBtn.setTitle("Recording", for: .normal)
+            print("Recording Started")
+        }
+        else{
+            recordBtn.setTitle("Record", for: .normal)
+            print("Recording Stoped")
+        }
+        isRecording = !isRecording
+    }
+    
 }
 
